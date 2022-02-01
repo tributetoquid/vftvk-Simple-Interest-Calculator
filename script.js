43d81b5a1d7d9963b88040238e67fca5892cefd2
Abbas Rahim <64686112+tributetoquid@users.noreply.github.com>
2022-02-01 10:24:08 +0500

    updated
    changed according to the instructions

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
+
+// a function to dynamically update rate value while changing
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value; // getting value from the rate field 
+    document.getElementById("rate_val").innerText=rateval+"%"; // assingning that above value to span element with id = rate_val
+}
+
+
+
+
+        
+// now computing the whole result
 function compute()
 {
-    p = document.getElementById("principal").value;
+
+    var principal = document.getElementById("principal").value; //getting value from amount field
+    // checking to see if user has entered zero or negative value
+    if (principal <=0) {
+        alert("Please Enter a positive Number!"); //showing a pop-window to the user
+        document.getElementById("principal").focus();
+         };
+    var rate = document.getElementById("rate").value; //taking value from rate field
+    var years = document.getElementById("year").value; // taking years 
+    var year = new Date().getFullYear()+parseInt(years); //converting years to integer and then adding the with current year
+    var amount = principal * year * rate /100; // calculating the total amount
+        //  now printing the result in the span element with id = result
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>";
+
     
 }
-        
+            
